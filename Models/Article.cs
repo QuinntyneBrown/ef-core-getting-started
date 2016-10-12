09e8cd25c17797c5345c0d870d7803526db52df6
Quinntyne Brown <quinntynebrown@gmail.com>
2016-10-11 20:34:48 -0400

    models

---
+++ Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace EfCoreGettingStarted.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string HtmlBody { get; set; }
+    }
+}
---
+++ Models/Blog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EfCoreGettingStarted.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Article> Articles { get; set; } = new HashSet<Article>();
+    }
+}
